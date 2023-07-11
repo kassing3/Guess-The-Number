c90b42bed4ea5b1b97a518d6efb871e29a562b33
Lupita Peralta <cstuart@email.com>
2023-07-11 15:42:33 -0500

    round/id path

--- guessTheNumber/src/main/java/com/G2/guessTheNumber/service/GameServiceImpl.java
+++ guessTheNumber/src/main/java/com/G2/guessTheNumber/service/GameServiceImpl.java
@@ -1,4 +1,22 @@
 package com.G2.guessTheNumber.service;
 
+import com.G2.guessTheNumber.dao.RoundDao;
+import com.G2.guessTheNumber.dto.Round;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameServiceImpl implements GameServiceInterface{
+    @Autowired
+    RoundDao roundDao;
+    
+    public GameServiceImpl(RoundDao roundDao) {
+        this.roundDao = roundDao;
+    }
+    
+    public List<Round> getAllRoundsById(int gameId) {
+        return roundDao.getAllRoundsById(gameId);
+    }
+
 }
