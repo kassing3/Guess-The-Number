c8a64ed9851d878c4a51bf85b2a567a4da478c87
Natalie Tang <natalietang77@gmail.com>
2023-07-12 12:16:12 -0500

    begin code -nat

--- guessTheNumber/src/main/java/com/G2/guessTheNumber/controller/GameController.java
+++ guessTheNumber/src/main/java/com/G2/guessTheNumber/controller/GameController.java
@@ -1,9 +1,36 @@
 package com.G2.guessTheNumber.controller;
 
+import com.G2.guessTheNumber.dao.GameDao;
+import com.G2.guessTheNumber.dao.RoundDao;
+import com.G2.guessTheNumber.dto.Game;
+import com.G2.guessTheNumber.service.GameServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class GameController {
+    private final GameDao gameDao;
+    private final RoundDao roundDao;
+
+    public GameController(GameDao gameDao, RoundDao roundDao) {
+        this.gameDao = gameDao;
+        this.roundDao = roundDao;
+    }
+
+    @PostMapping("/begin")
+    @ResponseStatus(HttpStatus.CREATED) //sets status to 201 when successfully created
+    public Game create() {
+        //instantiate GameServiceImpl to access game-related logic
+        GameServiceImpl gameService = new GameServiceImpl();
+        //generates new game using gameService's newGame() method
+        Game newGame = gameService.newGame();
+        //saves new game to database using gameDao's createGame() method
+        gameDao.createGame(newGame);
+//        return gameService.getGame(newGame.getGameId());
+        return gameService.newGame();
+    }
 }
--- guessTheNumber/src/main/java/com/G2/guessTheNumber/service/GameServiceImpl.java
+++ guessTheNumber/src/main/java/com/G2/guessTheNumber/service/GameServiceImpl.java
@@ -1,4 +1,26 @@
 package com.G2.guessTheNumber.service;
 
-public class GameServiceImpl implements GameServiceInterface{
+import com.G2.guessTheNumber.dto.Game;
+import com.G2.guessTheNumber.dto.Status;
+
+import java.util.Random;
+
+public class GameServiceImpl implements GameServiceInterface {
+
+    public Game newGame() {
+        Game game = new Game();
+        Random random = new Random();
+        //stores digits of the answer
+        StringBuilder answer = new StringBuilder();
+
+        //generates 4-digit answer for game
+        for (int i = 0; i < 4; i ++) {
+            int num = random.nextInt(10);
+            answer.append(num);
+        }
+        //sets answer and status of game
+        game.setAnswer(answer.toString());
+        game.setStatus(Status.IN_PROGRESS);
+        return game;
+    }
 }
